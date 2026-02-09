c3c38607fda2d7e84d8608d0e7eef2d243ea7e8d
biswashdipak603-tech <biswashdipak603@gmail.com>
2026-02-09 21:06:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,16 @@
-let pumpOn = false;
-let moisture = 50;
-let autoMode = true;
+const channelID = "YOUR_CHANNEL_ID";
+const readAPI = "YOUR_READ_API_KEY";
 
-function togglePump() {
-  if (!autoMode) {
-    pumpOn = !pumpOn;
-    updatePumpUI();
-  }
+function updateFromCloud() {
+  fetch(`https://api.thingspeak.com/channels/${channelID}/feeds/last.json?api_key=${readAPI}`)
+    .then(res => res.json())
+    .then(data => {
+      document.getElementById("moisture").innerText =
+        data.field1 + "%";
+
+      document.getElementById("pump").innerText =
+        data.field2 == 1 ? "ON" : "OFF";
+    });
 }
 
-function toggleMode() {
-  autoMode = !autoMode;
-  document.getElementById("mode").innerText = autoMode ? "AUTO" : "MANUAL";
-}
-
-function updatePumpUI() {
-  document.getElementById("pump").innerText = pumpOn ? "ON" : "OFF";
-}
-
-function updateMoisture() {
-  moisture = Math.floor(Math.random() * 100);
-  document.getElementById("moisture").innerText = moisture + "%";
-
-  if (autoMode) {
-    if (moisture < 30) {
-      pumpOn = true;
-    } else {
-      pumpOn = false;
-    }
-    updatePumpUI();
-  }
-}
-
-setInterval(updateMoisture, 2000);
+setInterval(updateFromCloud, 20000);
